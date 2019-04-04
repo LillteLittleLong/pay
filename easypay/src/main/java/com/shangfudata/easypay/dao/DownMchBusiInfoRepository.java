8f7578286c43955f636ca2fead48be4f3fb1a9f7
tinlly <luckstary@hotmail.com>
2019-04-04 16:54:21 +0800

    test

---
+++ easypay/src/main/java/com/shangfudata/easypay/dao/DownMchBusiInfoRepository.java
@@ -0,0 +1,32 @@
+package com.shangfudata.easypay.dao;
+
+import com.shangfudata.easypay.entity.DownMchBusiInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by tinlly to 2019/4/1
+ * Package for com.shangfu.pay.service.dao
+ */
+public interface DownMchBusiInfoRepository extends JpaRepository<DownMchBusiInfo, String>, JpaSpecificationExecutor<DownMchBusiInfo>, Serializable {
+
+    /**
+     * 获取该商户所对应的通道
+     * @return
+     */
+    @Query("from DownMchBusiInfo where down_sp_id = ?1 and down_mch_id = ?2 and down_mch_busi_type = ?3")
+    List<DownMchBusiInfo> queryMchPassage(String downSpId, String downMchId, String downMchBusiType);
+
+    /**
+     * 获取该商户所有的通道
+     * @param downMchId
+     * @param downMchBusiType
+     * @return
+     */
+    @Query("from DownMchBusiInfo where down_mch_id = ?1 and down_mch_busi_type = ?2")
+    List<DownMchBusiInfo> queryMchPassage(String downMchId, String downMchBusiType);
+
+}
---
+++ easypay/src/main/java/com/shangfudata/easypay/eureka/EurekaEasypayClient.java
@@ -0,0 +1,27 @@
+package com.shangfudata.easypay.eureka;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by tinlly to 2019/4/2
+ * Package for com.shangfudata.collpay.eureka
+ */
+@Service
+@FeignClient("routing-client")
+public interface EurekaEasypayClient {
+
+    /**
+     * 调用 routing 下游路由分发接口
+     */
+    @PostMapping("/downRouting")
+    String downRouting(@RequestParam(value = "downMchId") String downMchId, @RequestParam(value = "downSpId") String downSpId, @RequestParam(value = "totalFee") String totalFee, @RequestParam(value = "passage") String passage);
+
+    /**
+     * 调用 routing 上游路由分发接口
+     */
+    @PostMapping("/upRouting")
+    String upRouting(@RequestParam(value = "downSpId") String downSpId, @RequestParam(value = "mchId") String mchId, @RequestParam(value = "totalFee") String totalFee, @RequestParam(value = "passage") String passage);
+}
